fb70f58a54a36db0a3ad1f6dd94d4baa6599c8ed
Santiago Toquica Yanguas <santitoya-2001@outlook.com>
2024-09-17 18:32:21 -0500

    feat(tools-config-structure): KAN-12, Modify structure and update config.
    - Update structure to the project.- Update pre-push.
    - Add svg config to import.
    - Add tests config.
    - Install types and react query, notifications, luxon time.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -8,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
       <div>
         <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -27,7 +28,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
